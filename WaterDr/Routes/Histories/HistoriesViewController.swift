505a42a2daab127b2b719ae7446a8a37c27a63e2
ngr <>
2024-09-26 11:53:34 +0700

    update

---
+++ WaterDr/Routes/Histories/HistoriesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HistoriesViewController.swift
+//  WaterDr
+//
+//  Created by ngr on 26/09/24.
+//
+
+import Foundation
+import UIKit
+
+class HistoriesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
